ac8c960396d1f96538c65a5de386e99129295e2d
Adam Schalock <aschalock@splunk.com>
2023-10-24 15:25:09 -0700

    More test clean-up

--- tests/receivers/smartagent/telegraf-sqlserver/bundled_test.go
+++ tests/receivers/smartagent/telegraf-sqlserver/bundled_test.go
@@ -17,6 +17,7 @@
 package tests
 
 import (
+	"path"
 	"runtime"
 	"testing"
 
@@ -29,18 +30,18 @@ func TestMssqlDockerObserver(t *testing.T) {
 		t.Skip("unable to share sockets between mac and d4m vm: https://github.com/docker/for-mac/issues/483#issuecomment-758836836")
 	}
 
-	// server := testutils.NewContainer().WithContext(
-	// 	path.Join(".", "testdata", "server"),
-	// ).WithExposedPorts("1433:1433").WithName("sql-server").WithNetworks(
-	// 	"mssql",
-	// ).WillWaitForPorts("1433").WillWaitForLogs(
-	// 	"SQL Server is now ready for client connections.", "Recovery is complete.")
+	server := testutils.NewContainer().WithContext(
+		path.Join(".", "testdata", "server"),
+	).WithExposedPorts("1433:1433").WithName("sql-server").WithNetworks(
+		"mssql",
+	).WillWaitForPorts("1433").WillWaitForLogs(
+		"SQL Server is now ready for client connections.", "Recovery is complete.")
 
-	// client := testutils.NewContainer().WithContext(
-	// 	path.Join(".", "testdata", "client"),
-	// ).WithName("sql-client").WithNetworks("mssql").WillWaitForLogs("name", "signalfxagent")
+	client := testutils.NewContainer().WithContext(
+		path.Join(".", "testdata", "client"),
+	).WithName("sql-client").WithNetworks("mssql").WillWaitForLogs("name", "signalfxagent")
 
-	// containers := []testutils.Container{server, client}
+	containers := []testutils.Container{server, client}
 
 	//testutils.AssertAllMetricsReceived(t, "all.yaml", "all_metrics_config.yaml", containers, nil)
 	testutils.AssertAllMetricsReceived(t, "bundled.yaml", "otlp_exporter.yaml",
@@ -49,26 +50,25 @@ func TestMssqlDockerObserver(t *testing.T) {
 				cc := c.(*testutils.CollectorContainer)
 				cc.Container = cc.Container.WithBinds("/var/run/docker.sock:/var/run/docker.sock:ro")
 				return cc
-			}
-			// ,
-			// func(collector testutils.Collector) testutils.Collector {
-			// 	return collector.WithEnv(map[string]string{
-			// 		"MSSQL_URL":                 "tcp:sql-server,1433",
-			// 		"SPLUNK_DISCOVERY_DURATION": "10s",
-			// 		// confirm that debug logging doesn't affect runtime
-			// 		"SPLUNK_DISCOVERY_LOG_LEVEL": "debug",
-			// 		"splunk.discovery.default":   "Password!",
-			// 		"HOSTNAME":                   "sql.example.com",
-			// 		"login.name":                 "signalfxagent",
-			// 	}).WithArgs(
-			// 		"--discovery",
-			// 		"--set", "splunk.discovery.receivers.mssql.config.endpoint=localhost:1433",
-			// 		"--set", `splunk.discovery.extensions.k8s_observer.enabled=false`,
-			// 		"--set", `splunk.discovery.extensions.host_observer.enabled=false`,
-			// 		"--set", `splunk.discovery.extentsion.mssql.config.username=SA_ADMIN`,
-			// 		"--set", `splunk.discovery.extentsion.mssql.config.login.name=signalfxagent`,
-			// 	)
-			// },
+			},
+			func(collector testutils.Collector) testutils.Collector {
+				return collector.WithEnv(map[string]string{
+					"MSSQL_URL":                 "tcp:sql-server,1433",
+					"SPLUNK_DISCOVERY_DURATION": "10s",
+					// confirm that debug logging doesn't affect runtime
+					"SPLUNK_DISCOVERY_LOG_LEVEL": "debug",
+					"splunk.discovery.default":   "Password!",
+					"HOSTNAME":                   "sql.example.com",
+					"login.name":                 "signalfxagent",
+				}).WithArgs(
+					"--discovery",
+					"--set", "splunk.discovery.receivers.mssql.config.endpoint=localhost:1433",
+					"--set", `splunk.discovery.extensions.k8s_observer.enabled=false`,
+					"--set", `splunk.discovery.extensions.host_observer.enabled=false`,
+					"--set", `splunk.discovery.receivers.mssql.config.username=SA_ADMIN`,
+					"--set", `splunk.discovery.receivers.mssql.config.login.name=signalfxagent`,
+				)
+			},
 		},
 	)
 }
