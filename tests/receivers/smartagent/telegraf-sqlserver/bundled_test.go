dc81b7deaade9624fa8a82ad4474143fa47f8d0a
Adam Schalock <aschalock@splunk.com>
2023-10-31 14:30:42 -0700

    refactoring and adding in service

--- tests/receivers/smartagent/telegraf-sqlserver/bundled_test.go
+++ tests/receivers/smartagent/telegraf-sqlserver/bundled_test.go
@@ -43,18 +43,18 @@ func TestMssqlDockerObserver(t *testing.T) {
 			},
 			func(collector testutils.Collector) testutils.Collector {
 				return collector.WithEnv(map[string]string{
-					"SPLUNK_DISCOVERY_DURATION": "20s",
+					"SPLUNK_DISCOVERY_DURATION": "10s",
 					// confirm that debug logging doesn't affect runtime
 					"SPLUNK_DISCOVERY_LOG_LEVEL": "debug",
 					"MSSQL_PASSWORD":             "Password!",
 				}).WithArgs(
 					"--discovery",
 					"--set", "splunk.discovery.receivers.sqlserver.config.endpoint=localhost:1433",
+					"--set", `splunk.discovery.receivers.sqlserver.config.username=signalfxagent`,
+					"--set", "splunk.discovery.receivers.sqlserver.config.password='${MS_SQL_PASSWORD}'",
+					"--set", "splunk.discovery.receivers.sqlserver.config.service=mssql",
 					"--set", `splunk.discovery.extensions.k8s_observer.enabled=false`,
 					"--set", `splunk.discovery.extensions.host_observer.enabled=false`,
-					"--set", `splunk.discovery.receivers.sqlserver.config.username=signalfxagent`,
-					"--set", `splunk.discovery.receivers.sqlserver.config.login.name=signalfxagent`,
-					"--set", `splunk.discovery.receivers.sqlserver.config.password='${MS_SQL_PASSWORD}'`,
 				)
 			},
 		},
